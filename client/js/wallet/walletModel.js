b00eb8e2e8726993dd55229183851e2e7e76ef57
passby_time <passby_time@naver.com>
2020-03-12 15:59:43 +0900

    feat. Add useButton
    constructor에 초기 값 데이터 설정(0) -> 추후 데이터 값 추가 예정
    값이 1씩 감소하는 useButton 기능 추가

--- client/js/wallet/walletModel.js
+++ client/js/wallet/walletModel.js
@@ -3,6 +3,12 @@ import Observable from "../observable.js";
 class WalletModel extends Observable {
   constructor() {
     super();
+    this.state = 0; // 초기 값 데이터 추가 예정
+  }
+
+  useButton() {
+    this.state -= 1;
+    this.notify(this.state);
   }
 }
 
