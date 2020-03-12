7190e31bc6e48af8125f141bc40bbe94f937c17c
passby_time <passby_time@naver.com>
2020-03-12 15:52:08 +0900

    feat. Add querySelector, Add new operator
    사용할 querySelector, 객체 생성

--- client/js/wallet/walletMain.js
+++ client/js/wallet/walletMain.js
@@ -1,2 +1,9 @@
 import WalletView from "./walletView.js";
 import WalletModel from "./walletModel.js";
+
+const useButton = document.querySelectorAll(".money-unit");
+const root = document.querySelector(".money-wrap span");
+const totalMoney = document.querySelector(".total-money");
+
+const walletModel = new WalletModel();
+const walletView = new WalletView(walletModel, root, totalMoney);
