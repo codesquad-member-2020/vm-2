978eb2931a68a5327a55382d520d29d6b6a07a22
choisohyun <sohyun4283@naver.com>
2020-03-17 12:38:09 +0900

    fix. Change import file path

--- client/js/main.js
+++ client/js/main.js
@@ -1,9 +1,10 @@
-import WalletView from "./walletView.js";
-import WalletModel from "./walletModel.js";
-import ProductListModel from "../productList/productListModel.js";
-import ProductListView from "../productList/productListView.js";
+import WalletView from "./wallet/walletView.js";
+import WalletModel from "./wallet/walletModel.js";
+import ProductListModel from "./productList/productListModel.js";
+import ProductListView from "./productList/productListView.js";
 
 const moneyWrap = document.querySelector(".money-wrap");
+const productWrap = document.querySelector(".product");
 
 const walletModel = new WalletModel();
 const productListModel = new ProductListModel();
@@ -13,3 +14,5 @@ const productListView = new ProductListView(productListModel, walletModel);
 moneyWrap.addEventListener("click", event => {
   walletModel.deliverUnitMoney(event);
 });
+
+productWrap.addEventListener("click", event => productListModel.getSelectProductInfo(event));
